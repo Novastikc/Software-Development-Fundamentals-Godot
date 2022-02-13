466b47c8c30646576c24cacfcf9a05a03fe20642
Cam <0157863@schoolsnet.act.edu.au>
2022-02-14 10:30:21 +1100

    player.gd update
    The new code in player.gd allows the player to move at a moderate pace both left and right. A possible note for this is that if you hold left or right, they can go offscreen, fix in the future.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,13 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var movement_speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(true)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		move_and_collide(Vector2(-movement_speed * delta, 0))
+	if Input.is_action_pressed("ui_right"):
+		move_and_collide(Vector2(movement_speed * delta, 0))
