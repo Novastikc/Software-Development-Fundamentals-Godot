ae4b2f955cac9a178daa89a39a34ef029d85ce59
Cam <0157863@schoolsnet.act.edu.au>
2022-02-24 12:01:18 +1100

    all resprited, and halfway through making barriers
    all resprited, and halfway through making barriers.
    go to subject website and FIGURE IT OUT.

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,9 @@ var bulletSource = preload ("res://Bullet/Bullet.tscn")
 func _ready():
 	set_process(true)
 	set_physics_process(true)
+	$Area2D.connect("area_entered", self, "_colliding")
+	$Area2D.connect("area_exited", self, "_not_colliding")
+
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
@@ -19,3 +22,13 @@ func _process(delta):
 		var bulletInstance = bulletSource.instance()
 		bulletInstance.position = Vector2(position.x, position.y-20)
 		get_tree().get_root().add_child(bulletInstance)
+
+func _colliding(area):
+	if area.is_in_group("left"):
+		print("left")
+		position.x=-3
+	if area.is_in_group("right"):
+		print("right")
+	
+func _not_colliding(area):
+	pass
