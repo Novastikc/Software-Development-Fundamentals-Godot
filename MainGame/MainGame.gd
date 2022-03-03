ec976fb40a3610fb47ec670a4e8cadb240b5b140
Cam <0157863@schoolsnet.act.edu.au>
2022-03-03 11:55:32 +1100

    part1 (highscore is here)
    Updated game so that a high score hud is here.

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -11,6 +11,8 @@ func _ready():
 		yield(get_tree().create_timer(1.0), "timeout")
 		$HUD/Countdown.text = str(currentTimer)
 		$HUD/CurrentScore.text = str(GlobalVariables.scoringInformation["currentScore"])
+		$HUD/HighScore.text = str(GlobalVariables.scoringInformation["highScore"])
+		# High score from the dictonary in Global.gd needs to be updated somehow, but it's here.
 		currentTimer = currentTimer - 1
 		print(currentTimer)
 	print("Game Over")
