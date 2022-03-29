a9d9809df56335625f2053316810e84759326016
Cam <98795113+Camasaurus@users.noreply.github.com>
2022-03-29 15:18:23 +1100

    Main Menu Change
    Our projects have been updated to include a new scene, featuring a series of buttons soon to be implemented.

---
+++ Button.gd
@@ -0,0 +1,8 @@
+extends Button
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_Button_pressed():
+	GlobalVariables.scoringInformation["currentScore"] = 0
+	get_tree().change_scene("res://MainGame/MainGame.tscn")
--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -7,12 +7,23 @@ func _ready():
 	currentTimer = countdownMax
 	$HUD/Countdown.text = str(currentTimer)
 	
+	set_process(true)
 	while currentTimer > 0:
 		yield(get_tree().create_timer(1.0), "timeout")
 		$HUD/Countdown.text = str(currentTimer)
-		$HUD/CurrentScore.text = str(GlobalVariables.scoringInformation["currentScore"])
-		$HUD/HighScore.text = str(GlobalVariables.scoringInformation["highScore"])
 		# High score from the dictonary in Global.gd needs to be updated somehow, but it's here.
 		currentTimer = currentTimer - 1
 		print(currentTimer)
 	print("Game Over")
+	
+	if GlobalVariables.previousScores.size() < 5:
+		GlobalVariables.previousScores.push_front(GlobalVariables.scoringInformation["currentScore"])
+	else: 
+		GlobalVariables.previousScores.remove(4)
+		# This removes the fourth element of the high scores (0,1,2,3,4) <-
+		# a bug with this is that you need a new run per run to reset for newest score
+	print(GlobalVariables.previousScores)
+	get_tree().change_scene("res://EndScene.tscn")
+
+func _process(delta):
+	$HUD/CurrentScore.text = str(GlobalVariables.scoringInformation["currentScore"])
