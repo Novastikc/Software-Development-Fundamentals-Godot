6338efe94e488230165da5e3ad48301803733fcb
Oliver <98795113+Camasaurus@users.noreply.github.com>
2022-04-28 12:00:06 +1000

    beginner options menu stuff
    soon we will have control over 'rapid fire' and probably 'hard mode'

--- Bullet-Enemy/Bullet-Enemy.gd
+++ Bullet-Enemy/Bullet-Enemy.gd
@@ -1,16 +1,8 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var speed = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	GlobalVariables.enemyBulletInstanceCount += 1
+	set_physics_process(true)
