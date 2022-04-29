f5338c187b3ecba10971255fad73366a6366a1a9
Camasaurus <0157863@schoolsnet.act.edu.au>
2022-04-29 15:07:36 +1000

    Enemy Bullets Part 1
    Enemies can shoot bullets but Enemy.gd line 20 where yield finishes theres a problem there that significantly reduces game quality, get ryan to help pls thank you very much :/
    
    But yeah, enemies now shoot bullets, and they don't actually damage you yet. Nor do they go through other enemies, they just kind of clump together right now...

--- Bullet-Enemy/Bullet-Enemy.gd
+++ Bullet-Enemy/Bullet-Enemy.gd
@@ -6,3 +6,15 @@ var speed = 500
 func _ready():
 	GlobalVariables.enemyBulletInstanceCount += 1
 	set_physics_process(true)
+
+func _physics_process(delta):
+	var collidedObject = move_and_collide(Vector2(0, +speed*delta*0.4))
+	if (collidedObject):
+		#print("Enemy collide: ",collidedObject.collider.name)
+		if "Enemy" in collidedObject.collider.name:
+			pass
+			#collidedObject.get_collider().queue_free() #Don't kill the enemies.
+		else:
+			queue_free()
+			GlobalVariables.enemyBulletInstanceCount -= 1
+			print("Enemy Bullets: ", GlobalVariables.enemyBulletInstanceCount)
--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+var bullet = preload("res://Bullet-Enemy/Bullet-Enemy.tscn")
+
 func _ready():
 	$Area2D.connect("area_entered", self, "_colliding")
 
@@ -10,3 +12,14 @@ func _colliding(area):
 	if area.is_in_group("left"):
 		get_parent().global_position.y += 10
 		get_parent().speed = get_parent().speed * -1
+
+func _process(delta):
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var my_random_number = rng.randf_range(2.0,30.0)
+	yield(get_tree().create_timer(my_random_number), "timeout")
+	if GlobalVariables.enemyBulletInstanceCount < 5:
+		var bulletInstance = bullet.instance()
+		
+		bulletInstance.position = Vector2(global_position.x, global_position.y)
+		get_tree().get_root().add_child(bulletInstance)
